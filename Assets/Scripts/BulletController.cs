e76a8d225b020f938c1faba7091a19f0b5ba3723
BOT-Mojjo <mojjo.olander@gmail.com>
2023-05-02 12:31:48 +0200

    Jeff-Init

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+  [SerializeField]
+  float initialVelocity = 12;
+
+  [SerializeField]
+  float lifeTime = 5;
+
+	void Start ()
+  {
+    Rigidbody rigidbody = GetComponent<Rigidbody>();
+    rigidbody.velocity = transform.forward * initialVelocity;
+
+    Destroy(this.gameObject, lifeTime);
+	}
+}
